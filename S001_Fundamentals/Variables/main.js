e8a38b4c0678d923705220fb6b91359638be401f
Diego Alexandre <ddsa.1988@outlook.com>
2024-12-17 00:36:42 -0300

    commit

---
+++ S001_Fundamentals/Variables/main.js
@@ -0,0 +1,19 @@
+const PI = 3.14;
+const myName = "Diego";
+
+let number = 1;
+
+console.log(PI);
+console.log(myName);
+console.log(number);
+console.log();
+
+try {
+    myName = "Rodrigo";
+} catch (error) {
+    console.error(error);
+}
+
+number = 10.52;
+
+console.log(number);
